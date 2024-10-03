3aa6e82666fcd7449b9ba975d3785828f4caff9f
Gustavo Fernandes <gustavofernandes.rj@hotmail.com>
2024-10-03 20:31:44 -0300

    Seção 2 - Aula 24 - Comentários nos componentes

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,6 +1,15 @@
+// arquivo de estilo
+
 const FirstComponent = () => {
+  // essa função faz isso
+
+  /*
+    multi line
+  */
+
   return (
     <div>
+      {/* Algum comentário */}
       <h1>Meu primeiro componente</h1>
       <p className="teste">Meu Texto</p>
     </div>
