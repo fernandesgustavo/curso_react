f2e781a1f86beb634fffd5f410dc4b8c8446d178
Gustavo Fernandes <gustavofernandes.rj@hotmail.com>
2024-10-04 11:34:11 -0300

    Seção 2 - Aula 29 - Funções nos eventos

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -10,6 +10,22 @@ const Events = () => {
       <div>
         <button onClick={handleMyEvent}>Clique aqui!</button>
       </div>
+      <div>
+        <button onClick={() => console.log("Clicou!")}>
+          Clique aqui também!
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            if (true) {
+              console.log("Isso aqui não deveria existir =)");
+            }
+          }}
+        >
+          Clique aqui, por favor!
+        </button>
+      </div>
     </div>
   );
 };
