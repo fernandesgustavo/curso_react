b802f185917df1a2baa4e43f04091d43983d9e1a
Gustavo Fernandes <gustavofernandes.rj@hotmail.com>
2024-10-04 11:42:41 -0300

    Seção 2 - Aula 30 - Função de renderização

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -5,6 +5,14 @@ const Events = () => {
     console.log("Ativou o evento!");
   };
 
+  const renderSomething = (x) => {
+    if (x) {
+      return <h1>Renderizando isso!</h1>;
+    } else {
+      return <h1>Também posso renderizar isso!</h1>;
+    }
+  };
+
   return (
     <div>
       <div>
@@ -26,6 +34,8 @@ const Events = () => {
           Clique aqui, por favor!
         </button>
       </div>
+      {renderSomething(true)}
+      {renderSomething(false)}
     </div>
   );
 };
