7907253a96f09e96d86827b9275882e5627c0f69
Gustavo Fernandes <gustavofernandes.rj@hotmail.com>
2024-10-09 18:39:23 -0300

    Seção 3 - Aula 38 - A propriedade key

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
@@ -2,11 +2,25 @@ import { useState } from "react";
 
 const ListRender = () => {
   const [list] = useState(["Matheus", "Pedro", "Josias", "Maria"]);
+
+  const [users] = useState([
+    { id: 1, name: "Matheus", age: 31 },
+    { id: 123451347, name: "João", age: 28 },
+    { id: 575362343, name: "Pedro", age: 44 },
+  ]);
+
   return (
     <div>
       <ul>
-        {list.map((item) => (
-          <li>{item}</li>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age}
+          </li>
         ))}
       </ul>
     </div>
