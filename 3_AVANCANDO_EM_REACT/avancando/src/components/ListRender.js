60e3b69f9afc2c5ae94e64b73ee5593aec4cf3fa
Gustavo Fernandes <gustavofernandes.rj@hotmail.com>
2024-10-09 19:06:40 -0300

    Seção 3 - Aula 39 - Previous state

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
@@ -3,11 +3,19 @@ import { useState } from "react";
 const ListRender = () => {
   const [list] = useState(["Matheus", "Pedro", "Josias", "Maria"]);
 
-  const [users] = useState([
+  const [users, setUsers] = useState([
     { id: 1, name: "Matheus", age: 31 },
-    { id: 123451347, name: "João", age: 28 },
-    { id: 575362343, name: "Pedro", age: 44 },
+    { id: 2, name: "João", age: 28 },
+    { id: 3, name: "Pedro", age: 44 },
   ]);
+
+  const deleteRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 4);
+
+    setUsers((prevUsers) => {
+      return prevUsers.filter((user) => randomNumber !== user.id);
+    });
+  };
 
   return (
     <div>
@@ -23,6 +31,7 @@ const ListRender = () => {
           </li>
         ))}
       </ul>
+      <button onClick={deleteRandom}>Delete Random User</button>
     </div>
   );
 };
