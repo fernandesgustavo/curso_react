a8b5c7b8e3176901c08d99fd6d8dfec99dc5b918
Gustavo Fernandes <gustavofernandes.rj@hotmail.com>
2024-10-09 15:29:48 -0300

    Seção 3 - Aula 34 - Imagens em src

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import City from "./assets/city.jpg";
 
 function App() {
   return (
@@ -7,6 +8,10 @@ function App() {
       {/* Imagem em public */}
       <div>
         <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      {/* Imagem em assets */}
+      <div>
+        <img src={City} alt="Cidade" />
       </div>
     </div>
   );
