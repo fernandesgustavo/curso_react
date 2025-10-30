61b17954a19d12b208db10a7fd94b5f526ed527a
Gustavo Fernandes <gustavofernandes.rj@hotmail.com>
2025-10-30 19:00:05 -0300

    Seção 3 - Aula 43 - If e else no JSX

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.js
@@ -3,11 +3,25 @@ import { useState } from "react";
 const ConditionalRender = () => {
   const [x] = useState(false);
 
+  const [name, setName] = useState("João");
+
   return (
     <div>
       <h1>Isso será exibido?</h1>
       {x && <p>Se x for true, sim!</p>}
       {!x && <p>Agora x é falso</p>}
+      <h1>IF Ternário</h1>
+      {name === "Gustavo" ? (
+        <div>
+          <p>O nome é Gustavo!</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome não encontrado!</p>
+        </div>
+      )}
+
+      <button onClick={() => setName("Gustavo")}>Clica aqui!</button>
     </div>
   );
 };
