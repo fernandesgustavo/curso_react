de39cdd6e003f340710f4ccfdaa951564707769c
Gustavo Fernandes <gustavofernandes.rj@hotmail.com>
2025-10-30 19:13:06 -0300

    Seção 3 - Aula 44 - Introdução as props

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import City from "./assets/city.jpg";
 import ConditionalRender from "./components/ConditionalRender";
 import ListRender from "./components/ListRender";
 import ManageData from "./components/ManageData";
+import ShowUserName from "./components/ShowUserName";
 
 function App() {
+  const name = "Joaquim";
+  const [userName] = useState("Gustavo");
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -19,6 +24,7 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      <ShowUserName name={userName} />
     </div>
   );
 }
