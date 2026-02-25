1bdc40ebde5acb130fd3fd5d7d5935d8b328b32f
Gustavo Fernandes <gustavofernandes.rj@hotmail.com>
2026-02-25 11:46:55 -0300

    Seção 3 - Aula 50 - Função como prop

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -8,6 +8,7 @@ import ShowUserName from "./components/ShowUserName";
 import CarDetails from "./components/CarDetails";
 import Fragment from "./components/Fragment";
 import Container from "./components/Container";
+import ExecuteFunction from "./components/ExecuteFunction";
 
 function App() {
   //const name = "Joaquim";
@@ -18,6 +19,10 @@ function App() {
     { id: 2, brand: "KIA", color: "Branco", newCar: false, km: 34343 },
     { id: 3, brand: "Renault", color: "Azul", newCar: false, km: 234 },
   ];
+
+  function showMessage() {
+    console.log("Evento do componente pai!");
+  }
 
   return (
     <div className="App">
@@ -43,6 +48,7 @@ function App() {
       {/* loop em array de objetos */}
       {cars.map((car) => (
         <CarDetails
+          key={car.id}
           brand={car.brand}
           color={car.color}
           km={car.km}
@@ -58,6 +64,8 @@ function App() {
       <Container myValue="testing2">
         <h5>Testando o container</h5>
       </Container>
+      {/* executar função */}
+      <ExecuteFunction myFunction={showMessage} />
     </div>
   );
 }
